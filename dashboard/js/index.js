205922a8ebf04970e602268ca7d8975941c1405e
vmaragos <36047769+vmaragos@users.noreply.github.com>
2020-06-02 14:12:42 +0300

    Hover effects (works on theme switch)
    The hover effects were re-made with JavaScript in order to work after the website theme is switched.

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -133,3 +133,320 @@ function switchTheme(){
         document.getElementById("slider").style.backgroundImage = "linear-gradient(to bottom left, hsl(210, 78%, 56%), hsl(146, 68%, 55%)";
     }
 }
+
+//first row
+
+function changeBackColor0()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[0].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[0].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor0()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[0].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[0].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+function changeBackColor1()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[1].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[1].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor1()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[1].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[1].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+function changeBackColor2()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[2].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[2].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor2()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[2].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[2].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+function changeBackColor3()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[3].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[3].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor3()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-inner-top")[3].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-inner-top")[3].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+//second row
+
+function changeBackColor4()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[0].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[0].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor4()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[0].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[3].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+function changeBackColor5()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[1].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[1].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor5()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[1].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[1].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+function changeBackColor6()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[2].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[2].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor6()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[2].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[2].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+function changeBackColor7()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[3].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[3].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor7()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-1")[3].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-1")[3].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+//third row
+
+function changeBackColor8()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[0].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[0].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor8()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[0].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[0].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+function changeBackColor9()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[1].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[1].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor9()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[1].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[1].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+function changeBackColor10()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[2].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[2].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor10()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[2].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[2].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
+function changeBackColor11()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[3].style.backgroundColor = "rgb(51, 58, 86)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[3].style.backgroundColor = "rgb(230, 232, 245)";
+    }
+}
+
+function revertBackColor11()
+{
+    if (document.getElementById("theme-switch").checked == false)
+    {        
+        document.getElementsByClassName("tile-outer-bottom-row-2")[3].style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+
+    else if (document.getElementById("theme-switch").checked == true)
+    {
+        document.getElementsByClassName("tile-outer-bottom-row-2")[3].style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
+}
+
